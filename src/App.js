82d904130cc7d80cc1d223e27a7bb90636a3be39
Billy Eskein <billyeskein@gmail.com>
2025-02-06 15:45:51 +0300

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import NavBar from "./components/NavBar";
+import React, {useState, useEffect} from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const[users, setUsers] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/users")
+    .then(response => response.json())
+    .then(data => setUsers(data))
+    .catch(error => console.error(error))
+  },[])
   return (
     <>
       <header>
-        
+        <NavBar/>
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
